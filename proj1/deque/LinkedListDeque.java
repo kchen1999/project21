dd03a7ff3d86b3010406aad37645227693e6b048
Kevin Chen <kevin@Kevinui-MacBookPro.local>
2024-11-02 20:30:51 +1100

    created LinkedListDeque file and added required method declarations

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,50 @@
+package deque;
+
+public class LinkedListDeque<T> {
+
+    private int size;
+
+    public LinkedListDeque() {
+        size = 0;
+    }
+
+    public void addFirst(T item) {
+        return;
+    }
+
+    public void addLast(T item) {
+        return;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        return;
+    }
+
+    public T removeFirst() {
+        return null;
+    }
+
+    public T removeLast() {
+        return null;
+    }
+
+    public T get(int index) {
+        return null;
+    }
+
+    public T getRecursive(int index) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
